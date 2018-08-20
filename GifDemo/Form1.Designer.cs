c353d031df7b9ed7de2f26eeda6e9aefc92ad3bb
Joker0727 <1610779207@qq.com>
2018-08-20 16:36:13 +0800

    gifdemo 合并生成gif

--- GifDemo/Form1.Designer.cs
+++ GifDemo/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,22 +45,65 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "图片文件夹";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "导出目录";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(370, 167);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Gif";
+            this.Text = "J·Y·T-GIF";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- GifDemo/Form1.cs
+++ GifDemo/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Gif.Components;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,15 +24,96 @@ namespace GifDemo
         {
 
         }
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog targetFolder = new FolderBrowserDialog();
 
+            if (targetFolder.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = targetFolder.SelectedPath;
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog targetFolder = new FolderBrowserDialog();
+
+            if (targetFolder.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox2.Text = targetFolder.SelectedPath;
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            string ResourcesFolder = this.textBox1.Text;
+            string targetFolder = this.textBox2.Text;
+            int delayTime = 3000;
+
+            if (string.IsNullOrEmpty(ResourcesFolder) || string.IsNullOrEmpty(targetFolder))
+            {
+                MessageBox.Show("文件夹路径不能为空！", "J·Y·T");
+                return;
+            }
+
+            string[] picPathArr = ReadPic(ResourcesFolder);
+            CreateGif(picPathArr, delayTime, targetFolder + @"\jyt.gif");
 
         }
-
-        public void CreateGif()
+        /// <summary>
+        /// 生成Gif
+        /// </summary>
+        /// <param name="picPathArr"></param>
+        /// <param name="delayTime"></param>
+        /// <param name="outputFilePath"></param>
+        public static void CreateGif(string[] picPathArr, int delayTime, string outputFilePath)
         {
-          
+            AnimatedGifEncoder gif = new AnimatedGifEncoder();
+            gif.Start(outputFilePath);
+            gif.SetDelay(delayTime);//设置延迟         
+            gif.SetRepeat(0); //-1:不循环,0:始终循环
+            //gif.SetSize(300,200);
+
+            for (int i = 0, count = picPathArr.Length; i < count; i++)
+            {
+                gif.AddFrame(Image.FromFile(picPathArr[i]));
+            }
+            gif.Finish();
+
         }
+
+        /// <summary>
+        /// 提取Gif中的没一张图片
+        /// </summary>
+        public static void ExtractGif(string gifFullPath, string outputPath)
+        {
+            GifDecoder gifDecoder = new GifDecoder();
+            gifDecoder.Read(gifFullPath);
+            for (int i = 0, count = gifDecoder.GetFrameCount(); i < count; i++)
+            {
+                Image frame = gifDecoder.GetFrame(i); // frame i
+                frame.Save(outputPath + Guid.NewGuid().ToString() + ".png", ImageFormat.Png);
+            }
+        }
+        /// <summary>
+        /// 读取图片路径
+        /// </summary>
+        /// <param name="picFolder"></param>
+        /// <returns></returns>
+        public static string[] ReadPic(string picFolder)
+        {
+            if (!Directory.Exists(picFolder))
+                Directory.CreateDirectory(picFolder);
+
+            string[] tempArr = Directory.GetFiles(picFolder);
+            List<string> resultList = new List<string>();
+            foreach (var item in tempArr)
+            {
+                string ExteName = Path.GetExtension(item);
+                if (ExteName.ToLower() == ".jpg" || ExteName.ToLower() == ".png" || ExteName.ToLower() == ".bmp")
+                    resultList.Add(item);
+            }
+            return resultList.ToArray();
+        }
+
     }
 }
